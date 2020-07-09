689c380f669239f7ac0f13584f82b3c9d12fce46
jshields <shields.h.josh@gmail.com>
2020-07-09 10:18:19 -0500

    check-changed-files

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,35 @@ json_file="/tmp/prospector.json"
 # get prospector version
 prospector --version
 
+# determine changed files
+# TODO/FIXME: separate this into a separate action and pass the files as an input to this action
+BRANCH=$(git rev-parse HEAD)
+LAST_EXIT=$?
+if [ $LAST_EXIT -ne 0 ]; then
+    echo "Could not determine branch rev with non-zero exit code $LAST_EXIT"
+    exit 1
+fi
+
+BASE=$(git merge-base $BRANCH origin/dev)
+LAST_EXIT=$?
+if [ $LAST_EXIT -ne 0 ]; then
+    echo "Could not determine base branch rev with non-zero exit code $LAST_EXIT"
+    exit 1
+fi
+
+FILES=$(git diff --name-only $BASE $BRANCH -- '*.py')
+LAST_EXIT=$?
+if [ $LAST_EXIT -ne 0 ]; then
+    echo "Could not determine modified files with non-zero exit code $LAST_EXIT"
+    exit 1
+fi
+if [ -z "$FILES" ]; then
+    echo "No files for Prospector to check"
+    exit 0
+fi
+
 # run prospector, save exit code
-prospector -o json:"$json_file" -o text
+prospector -o json:"$json_file" -o text $FILES
 exit_code=$?
 
 # analyze json
